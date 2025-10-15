20f9d74856a89afbdbd7abc6fc867ff567d21d8e
tukinami seika <10forchette@gmail.com>
2025-10-16 07:18:41 +0900

    modify: add RSS for work-posts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({ outDir: "./docs" });
+export default defineConfig({
+       outDir: "./docs",
+       site: "https://tukinami.github.io"
+});
