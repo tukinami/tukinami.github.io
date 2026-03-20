f2eefdc4057cf2119cdf79a917a1b8c21b57d24d
tukinami seika <10forchette@gmail.com>
2026-03-20 17:15:47 +0900

    modify: update libraries.

---
+++ docs/_astro/BaseLayout.astro_astro_type_script_index_0_lang.DXR-qCL2.js
@@ -0,0 +1,1 @@
+import"https://cdn.jsdelivr.net/npm/@nikolat/makibishi@0.2.0";
--- src/pages/work-posts.xml.js
+++ src/pages/work-posts.xml.js
@@ -1,12 +1,13 @@
 import rss, { pagesGlobToRssItems }from "@astrojs/rss";
 
 export async function GET(context) {
+    const items = await pagesGlobToRssItems(
+            import.meta.glob("./works-posts/*.md"),
+        );
     return rss({
         title: "tukinami.github.io - 制作物",
         description: "月波清火の制作物のRSS。",
         site: context.site,
-        items: await pagesGlobToRssItems(
-            import.meta.glob("./works-posts/*.md"),
-        ),
+        items: items,
     });
 }
